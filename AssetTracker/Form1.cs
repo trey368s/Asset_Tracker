0cfacbd4dc33805d1b02b86cf8c8a8a4a8fc199d
trey368s <84042739+trey368s@users.noreply.github.com>
2022-12-16 15:44:52 -0500

    Add WMI queries

--- AssetTracker/Form1.cs
+++ AssetTracker/Form1.cs
@@ -9,6 +9,8 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using static System.Net.Mime.MediaTypeNames;
 using System.Management;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+using System.Reflection;
 
 namespace AssetTracker
 {
@@ -24,22 +26,51 @@ namespace AssetTracker
 
             try
             {
-                ManagementObjectSearcher searcher =
+                ManagementObjectSearcher searcherUsername =
+                    new ManagementObjectSearcher("root\\CIMV2",
+                    "SELECT * FROM Win32_ComputerSystem");
+                foreach (ManagementObject queryObj in searcherUsername.Get())
+                {
+                    var domainName = queryObj["UserName"].ToString();
+                    var userName = domainName.Substring(domainName.LastIndexOf(@"\") + 1); 
+                }
+                ManagementObjectSearcher searcherModel =
                     new ManagementObjectSearcher("root\\CIMV2",
                     "SELECT * FROM Win32_ComputerSystem");
 
-                foreach (ManagementObject queryObj in searcher.Get())
+                foreach (ManagementObject queryObj in searcherModel.Get())
                 {
-                    Console.WriteLine("-----------------------------------");
-                    Console.WriteLine("Win32_ComputerSystem instance");
-                    Console.WriteLine("-----------------------------------");
-                    Console.WriteLine("UserName: {0}", queryObj["UserName"]);
-                    label1.Text = queryObj["UserName"].ToString();
+                    var model = queryObj["Model"].ToString();
                 }
+                ManagementObjectSearcher searcherSN =
+                    new ManagementObjectSearcher("root\\CIMV2",
+                    "SELECT * FROM Win32_BIOS");
+
+                foreach (ManagementObject queryObj in searcherSN.Get())
+                {
+                    var serialNumber = queryObj["SerialNumber"].ToString();
+                }
+                ManagementObjectSearcher searcherName =
+                    new ManagementObjectSearcher("root\\CIMV2",
+                    "SELECT * FROM Win32_ComputerSystem");
+
+                foreach (ManagementObject queryObj in searcherName.Get())
+                {
+                    var computerName = queryObj["Name"].ToString();
+                }
+                ManagementObjectSearcher searcherMan =
+                    new ManagementObjectSearcher("root\\CIMV2",
+                    "SELECT * FROM Win32_ComputerSystem");
+
+                foreach (ManagementObject queryObj in searcherMan.Get())
+                {
+                    var manufacturer = queryObj["Manufacturer"].ToString();
+                }
+
             }
             catch (ManagementException )
             {
-                MessageBox.Show("An error occurred while querying for WMI data. ");
+                MessageBox.Show("An error occurred while querying for WMI data.");
             }
         }
     }
