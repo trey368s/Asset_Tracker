9a0ad81abceee5709522decabdaab273920e0636
trey368s <84042739+trey368s@users.noreply.github.com>
2022-12-19 15:05:40 -0500

    Assign user to ticket

--- AssetTracker/Form1.cs
+++ AssetTracker/Form1.cs
@@ -15,6 +15,7 @@ using System.Threading;
 using RestSharp;
 using RestSharp.Authenticators;
 using AssetTracker.Properties;
+using System.Xml.Linq;
 
 namespace AssetTracker
 {
@@ -28,13 +29,11 @@ namespace AssetTracker
         private void button1_Click(object sender, EventArgs e)
         {
 
-            try
-            {
+            try{
                 ManagementObjectSearcher searcherUsername =
                     new ManagementObjectSearcher("root\\CIMV2",
                     "SELECT * FROM Win32_ComputerSystem");
-                foreach (ManagementObject queryObj in searcherUsername.Get())
-                {
+                foreach (ManagementObject queryObj in searcherUsername.Get()){
                     var domainName = queryObj["UserName"].ToString();
                     var userName = domainName.Substring(domainName.LastIndexOf(@"\") + 1);
                     labelUsername.Text = userName;
@@ -43,8 +42,7 @@ namespace AssetTracker
                 ManagementObjectSearcher searcherModel =
                     new ManagementObjectSearcher("root\\CIMV2",
                     "SELECT * FROM Win32_ComputerSystem");
-                foreach (ManagementObject queryObj in searcherModel.Get())
-                {
+                foreach (ManagementObject queryObj in searcherModel.Get()){
                     var model = queryObj["Model"].ToString();
                     labelModel.Text = model;
                 }
@@ -52,8 +50,7 @@ namespace AssetTracker
                 ManagementObjectSearcher searcherSN =
                     new ManagementObjectSearcher("root\\CIMV2",
                     "SELECT * FROM Win32_BIOS");
-                foreach (ManagementObject queryObj in searcherSN.Get())
-                {
+                foreach (ManagementObject queryObj in searcherSN.Get()){
                     var serialNumber = queryObj["SerialNumber"].ToString();
                     labelSN.Text = serialNumber;
                 }
@@ -61,8 +58,7 @@ namespace AssetTracker
                 ManagementObjectSearcher searcherManufacturer =
                     new ManagementObjectSearcher("root\\CIMV2",
                     "SELECT * FROM Win32_ComputerSystem");
-                foreach (ManagementObject queryObj in searcherManufacturer.Get())
-                {
+                foreach (ManagementObject queryObj in searcherManufacturer.Get()){
                     var manufacturer = queryObj["Manufacturer"].ToString();
                     labelManufacturer.Text = manufacturer;
                 }
@@ -70,41 +66,32 @@ namespace AssetTracker
                 ManagementObjectSearcher searcherName =
                     new ManagementObjectSearcher("root\\CIMV2",
                     "SELECT * FROM Win32_ComputerSystem");
-                foreach (ManagementObject queryObj in searcherName.Get())
-                {
+                foreach (ManagementObject queryObj in searcherName.Get()){
                     var computerName = queryObj["Name"].ToString();
                     string location = computerName.Substring(0, 3);
                     labelName.Text = computerName;
-                    if (location == "COR")
-                    {
+                    if (location == "COR"){
                         labelLocation.Text = "Corporate";
                     }
-                    if (location == "CIN")
-                    {
+                    if (location == "CIN"){
                         labelLocation.Text = "Cincinnati";
                     }
-                    if (location == "IRV")
-                    {
+                    if (location == "IRV") {
                         labelLocation.Text = "Irving";
                     }
-                    if (location == "KAL")
-                    {
+                    if (location == "KAL"){
                         labelLocation.Text = "Kalamazoo";
                     }
-                    if (location == "MAN")
-                    {
+                    if (location == "MAN"){
                         labelLocation.Text = "Mankato";
                     }
-                    if (location == "RED")
-                    {
+                    if (location == "RED"){
                         labelLocation.Text = "Redmond";
                     }
-                    if (location == "SLC")
-                    {
+                    if (location == "SLC"){
                         labelLocation.Text = "Salt Lake City";
                     }
-                    if (location == "HMF")
-                    {
+                    if (location == "HMF"){
                         labelLocation.Text = "HMF Express";
                     }
                 }
@@ -114,22 +101,66 @@ namespace AssetTracker
                 textBoxTicket.Enabled = true;
                 buttonPost.Enabled = true;
             }
-            catch (ManagementException)
-            {
+            catch (ManagementException){
                 MessageBox.Show("An error occurred while querying for WMI data.");
             }
         }
 
         private void buttonPost_Click(object sender, EventArgs e)
         {
-            var shipClient = new RestClient("https://shsupport.jitbit.com/helpdesk/api/Asset"){
+            var createClient = new RestClient("https://shsupport.jitbit.com/helpdesk/api/Asset"){
                 Authenticator = new HttpBasicAuthenticator("tstegeman@senneca.com", "Password1")
             }; 
-            var shipRequest = new RestRequest(Method.POST);
-            shipRequest.AddParameter("modelName", "aaaTest");
-            IRestResponse shipResponse = shipClient.Execute(shipRequest); 
-            var shipResp = shipResponse.Content;
-            MessageBox.Show(shipResp);
+            var createRequest = new RestRequest(Method.POST);
+            createRequest.AddParameter("modelName", "aaaTest");
+            createRequest.AddParameter("manufacturer", labelManufacturer.Text);
+            createRequest.AddParameter("type", comboBoxType.Text);
+            createRequest.AddParameter("supplier", "Purchased");
+            createRequest.AddParameter("serialNumber", labelSN.Text);
+            createRequest.AddParameter("location", labelLocation.Text);
+            createRequest.AddParameter("comments", labelName.Text);
+            createRequest.AddParameter("company", "Senneca");
+            IRestResponse createResponse = createClient.Execute(createRequest); 
+            var createResp = createResponse.Content;
+            var createJson = Newtonsoft.Json.Linq.JObject.Parse(createResp); 
+            var id = (int)createJson["id"];
+
+            var ticketClient = new RestClient("https://shsupport.jitbit.com/helpdesk/api/AddAssetToTicket") {
+                Authenticator = new HttpBasicAuthenticator("tstegeman@senneca.com", "Password1")
+            };
+            var ticketRequest = new RestRequest(Method.POST);
+            ticketRequest.AddParameter("assetId", id);
+            ticketRequest.AddParameter("ticketId", textBoxTicket.Text);
+            IRestResponse ticketResponse = ticketClient.Execute(ticketRequest);
+
+            var userClient = new RestClient("https://shsupport.jitbit.com/helpdesk/api/UserByEmail"){
+                Authenticator = new HttpBasicAuthenticator("tstegeman@senneca.com", "Password1")
+            };
+            var userRequest = new RestRequest(Method.GET);
+            var email = "";
+            if (labelLocation.Text == "Salt Lake City"){
+                email = labelUsername.Text + "@subzeroeng.com";
+            }
+            if (labelLocation.Text == "Mankato") {
+                email = labelUsername.Text + "@doorengineering.com";
+            }
+            else{
+                email = labelUsername.Text + "@senneca.com";
+            }
+            userRequest.AddParameter("email",  email);
+            IRestResponse userResponse = userClient.Execute(userRequest);
+            var userResp = userResponse.Content;
+            var userJson = Newtonsoft.Json.Linq.JObject.Parse(userResp);
+            var userId = (int)userJson["UserID"];
+
+            var assignClient = new RestClient("https://shsupport.jitbit.com/helpdesk/api/AssignAssetToUser"){
+                Authenticator = new HttpBasicAuthenticator("tstegeman@senneca.com", "Password1")
+            };
+            var assignRequest = new RestRequest(Method.POST);
+            assignRequest.AddParameter("assetId", id);
+            assignRequest.AddParameter("userId", userId);
+            IRestResponse assignResponse = assignClient.Execute(assignRequest);
+            var assignResp = assignResponse.Content;
         }
     }
 }
